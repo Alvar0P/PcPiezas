a8adcc5c9c2824ea3320b57841d7fc780f33b7d9
Alvar0P <98239996+Alvar0P@users.noreply.github.com>
2023-02-07 13:28:08 +0100

    Initial Commit

---
+++ PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/PcPiezasApplication.java
@@ -0,0 +1,13 @@
+package com.AlvaroyRaul.PcPiezas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPiezasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcPiezasApplication.class, args);
+	}
+
+}
---
+++ PcPiezasIJ/src/test/java/com/AlvaroyRaul/PcPiezas/PcPiezasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlvaroyRaul.PcPiezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcPiezasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
