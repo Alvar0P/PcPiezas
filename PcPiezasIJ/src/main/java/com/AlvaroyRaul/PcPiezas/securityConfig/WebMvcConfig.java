6b23ff3f5bd06e2ae12283aa6a87e061d0f7af9b
Alvar0P <98239996+Alvar0P@users.noreply.github.com>
2023-05-01 12:45:15 +0200

    Cambios configuration

--- PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/securityConfig/WebMvcConfig.java
+++ PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/securityConfig/WebMvcConfig.java
@@ -1,8 +1,15 @@
 package com.AlvaroyRaul.PcPiezas.securityConfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -11,5 +18,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new WebMvcConfig.CookieHeaderInterceptor());
+    }
+
+    private static class CookieHeaderInterceptor extends HandlerInterceptorAdapter {
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                               ModelAndView modelAndView) throws Exception {
+            if (modelAndView != null) {
+                response.addCookie(new Cookie("JSESSIONID", request.getSession().getId()));
+            }
+        }
     }
 }
