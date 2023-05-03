1d652a232cd9e3b361926ec7672dbf279a23df88
Rarp14 <104164229+Rarp14@users.noreply.github.com>
2023-05-03 23:55:41 +0200

    cambios

--- PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/ProductoRepo.java
+++ PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/ProductoRepo.java
@@ -2,6 +2,7 @@ package com.AlvaroyRaul.PcPiezas.database.repository;
 import com.AlvaroyRaul.PcPiezas.database.entity.Producto;
 import com.AlvaroyRaul.PcPiezas.database.entity.Usuario;
 import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Repository
 @Transactional
+@CacheConfig(cacheNames = "usuarios")
 public interface ProductoRepo extends JpaRepository<Producto, Long> {
 
     @Cacheable
