936feb7d41910688e8ebf5fd1a175669859ed237
Alvar0P <98239996+Alvar0P@users.noreply.github.com>
2023-05-04 01:31:57 +0200

    Arreglos cache
    Co-Authored-By: Rarp14 <104164229+Rarp14@users.noreply.github.com>

--- PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/ItemRepo.java
+++ PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/ItemRepo.java
@@ -1,19 +1,26 @@
 package com.AlvaroyRaul.PcPiezas.database.repository;
 
+import com.AlvaroyRaul.PcPiezas.database.entity.Carrito;
 import com.AlvaroyRaul.PcPiezas.database.entity.Item;
 import com.AlvaroyRaul.PcPiezas.database.entity.Producto;
 import com.AlvaroyRaul.PcPiezas.database.entity.Venta;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-
+@CacheConfig(cacheNames = "usuarios")
 public interface ItemRepo extends JpaRepository<Item,String> {
 
     @Query("SELECT i FROM Item i WHERE i.Producto = ?1")
     List<Item> findByProducto(Producto Producto);
 
     List<Item> findByVenta(Venta venta);
+
+    @CacheEvict
+    Item save(Item item);
+
 
 }
--- PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/VentaRepo.java
+++ PcPiezasIJ/src/main/java/com/AlvaroyRaul/PcPiezas/database/repository/VentaRepo.java
@@ -1,10 +1,20 @@
 package com.AlvaroyRaul.PcPiezas.database.repository;
 
+import com.AlvaroyRaul.PcPiezas.database.entity.Carrito;
 import com.AlvaroyRaul.PcPiezas.database.entity.Usuario;
 import com.AlvaroyRaul.PcPiezas.database.entity.Venta;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@CacheConfig(cacheNames = "usuarios")
 public interface VentaRepo extends JpaRepository<Venta,Long> {
 
+    @CacheEvict
     void deleteByComprador(Usuario comprador);
+
+    @CacheEvict
+    Venta save(Venta venta);
+    @CacheEvict
+    Venta deleteById(long id);
 }
